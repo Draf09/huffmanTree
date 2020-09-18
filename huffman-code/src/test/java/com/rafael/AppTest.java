954c51fa2bd51a5b01d3c760b8760f9cde3e42aa
Draf <fontanadias@gmail.com>
2020-09-18 08:50:54 -0300

    Last before deliver.

--- huffman-code/src/test/java/com/rafael/AppTest.java
+++ huffman-code/src/test/java/com/rafael/AppTest.java
@@ -5,6 +5,7 @@
 package com.rafael;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
@@ -28,8 +29,38 @@ public class AppTest
 				&& result.get('o').equals("01");
 		
 		assertTrue(testResult);
-    }
+	}
+	
 
+	@Test
+	public void codeTest() {	
+		Map<Character,String> map = new HashMap<Character,String>();
+		map = HuffmanCode.createHuffmanCode1("Hello");
+		assertTrue(map.containsKey('H'));
+		assertTrue(map.containsKey('e'));
+		assertTrue(map.containsKey('l'));
+		assertTrue(map.containsKey('o'));
+	}
+
+	@Test
+	public void createCodeWithTableTest() {
+		Map<Character,String> outputMap = new HashMap<Character,String>();
+		Map<Character,String> inputMap = new HashMap<Character,String>();
+		inputMap.put('C', new String("1"));
+		inputMap.put('o', new String("3"));
+		inputMap.put('l', new String("1"));
+		inputMap.put('r', new String("1"));
+		inputMap.put('a', new String("1"));
+		inputMap.put('d', new String("1"));
+		outputMap = HuffmanCode.createHuffmanCode(inputMap);
+		assertTrue(outputMap.containsKey('C'));
+		assertTrue(outputMap.containsKey('o'));
+		assertTrue(outputMap.containsKey('l'));
+		assertTrue(outputMap.containsKey('r'));
+		assertTrue(outputMap.containsKey('a'));
+		assertTrue(outputMap.containsKey('d'));	
+	}
+	
     private void assertTrue(boolean testResult) {
 
     }
---
+++ huffman-code/src/test/java/com/rafael/HuffmanCode.java
@@ -0,0 +1,27 @@
+package com.rafael;
+
+import java.util.Map;
+
+public class HuffmanCode {
+
+	public static Map<Character, String> createHuffmanCode(Map<Character, String> inputMap) {
+		return null;
+	}
+
+	public static Map<Character, String> createHuffmanCode(Map<Character, Double> inputMap) {
+		return null;
+	}
+
+	public static Map<Character, String> createHuffmanCode(Map<Character, String> inputMap) {
+		return null;
+	}
+
+	public static Map<Character, String> createHuffmanCode(Map<Character, String> inputMap) {
+		return null;
+	}
+
+	public static Map<Character, String> createHuffmanCode1(String string) {
+		return null;
+	}
+
+}
